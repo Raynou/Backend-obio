81c23f24c15790e9f03e9426538f32454185255e
voidexiled <l20070518@cdmadero.tecnm.mx>
2021-07-07 05:08:44 -0500

    .

--- conf/common.py
+++ conf/common.py
@@ -101,4 +101,4 @@ AUTH_USER_MODEL = 'apiusers.User'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
